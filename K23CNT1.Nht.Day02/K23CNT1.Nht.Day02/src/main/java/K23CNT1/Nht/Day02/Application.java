4b5511f86b3773b190dfad9f4055281d8df0c205
Huutuyen205 <Huutuyen123tc@gmail.com>
2025-10-21 09:25:21 +0700

    day02

---
+++ K23CNT1.Nht.Day02/K23CNT1.Nht.Day02/src/main/java/K23CNT1/Nht/Day02/Application.java
@@ -0,0 +1,14 @@
+package K23CNT1.Nht.Day02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+        System.out.printf("gvhjjhghjg");
+	}
+
+}
